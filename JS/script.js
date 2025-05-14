7d3aa7cd6d1e49a3b391b979ec080218a3448f47
Divyansh Rajput <divyanshrajput91565@gmail.com>
2025-05-14 22:45:46 +0530

    Basic Stuff Changes.....

--- JS/script.js
+++ JS/script.js
@@ -138,7 +138,7 @@ function setStatus(isLive) {
 async function fetchSensorData() {
     // Handle test mode
     if (espIP === "test") {
-        updateSensorData(70, 18, 1);
+        updateSensorData(35, 28, 1);
         setStatus(true);
         lastUpdateTime = Date.now();
         return;
